514ee06f8df68acf387095e473a94d6d70b76793
Evan Berger <eberger@k0r4p4.42mulhouse.fr>
2023-07-06 14:37:32 +0200

    0.5 gestion de plus d'erreurs

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: eberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 11:26:20 by eberger           #+#    #+#             */
-/*   Updated: 2023/06/19 23:03:30 by eberger          ###   ########.fr       */
+/*   Updated: 2023/07/06 14:33:11 by eberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,19 @@ void	print_error_red(char *str)
 static int	argv_are_num(int argc, char **argv)
 {
 	int	i;
+	int	j;
 
 	i = 1;
 	while (i < argc)
 	{
-		if (!ft_isdigit(argv[i][0]))
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!ft_isdigit(argv[i][j]))
+				return (0);
+			j++;
+		}
+		if (ft_atoi(argv[i]) < 1 || ft_strlen(argv[i]) > 11)
 			return (0);
 		i++;
 	}
@@ -39,14 +47,14 @@ static void	example(void)
 	ft_putendl_fd("Pour que le programme fonctionne, procédé comme cela:", 1);
 	ft_putendl_fd("./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]", 1);
 	ft_putendl_fd("Exemple:", 1);
-	ft_putendl_fd("\t./philo 2 20 2 5 3", 1);
+	ft_putendl_fd("\t./philo 2 800 200 200 7", 1);
 }
 
 static int	error_argv_are_int(int argc, char **argv)
 {
 	if (argv_are_num(argc, argv))
 		return (0);
-	print_error_red("certains arguments spécifier ne sont pas des nombres");
+	print_error_red("certains arguments spécifier ne sont pas des int positifs");
 	example();
 	return (1);
 }
